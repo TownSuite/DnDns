54c6dfb0663e3acdad8acabe304017eb913d8fd8
Peter Gill <peter.gill@townsuite.com>
2019-06-27 14:06:41 -0230

    convert to csproj 2017 format and generate nuget package on build

--- SourceCode/DnDns/DnDns/Properties/AssemblyInfo.cs
+++ SourceCode/DnDns/DnDns/Properties/AssemblyInfo.cs
@@ -36,18 +36,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("DnDns")]
-[assembly: AssemblyDescription("A .NET DNS client.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("j. montgomery")]
-[assembly: AssemblyProduct("DnDns")]
-[assembly: AssemblyCopyright("Copyright © j. montgomery 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -55,16 +43,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cea06689-f515-4097-ae17-6eeef1d758ea")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
